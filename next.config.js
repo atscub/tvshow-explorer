11f550219a0d1e687022511fc8a3c3cc8286c30d
Abraham <abrahamtoledo90@gmail.com>
2023-08-07 19:12:36 +0200

    Allow any origin for images in development env

--- next.config.js
+++ next.config.js
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
 
-module.exports = nextConfig
+const remotePatterns =
+  process.env.NODE_ENV == "development"
+    ? [
+        {
+          protocol: "https",
+          hostname: "**",
+        },
+      ]
+    : [];
+
+const nextConfig = {
+  images: {
+    remotePatterns: remotePatterns,
+  },
+};
+
+module.exports = nextConfig;
